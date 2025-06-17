8276e50973d92db1c5260a8f577b7f3b7052bc3d
Andrzej Popek <andrzejpopek93@gmail.com>
2025-06-17 16:42:45 +0200

    add article deletion

--- src/app/articles/article-details-page/article-details-page.ts
+++ src/app/articles/article-details-page/article-details-page.ts
@@ -1,12 +1,26 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { doc, docData, Firestore } from '@angular/fire/firestore';
+import {
+  deleteDoc,
+  doc,
+  docData,
+  DocumentData,
+  DocumentReference,
+  Firestore,
+} from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { IArticle } from '../articles.interface';
 import { EmptyPage } from '../../ui/empty-page/empty-page-page';
 import { ArticleDetails } from '../article-details/article-details';
 import { articleConverter } from '../articles.converter';
+import { ToastrService } from 'ngx-toastr';
+import { GENERAL_ERROR_MESSAGE } from '../../util/messages';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  ConfirmDialog,
+  ConfirmDialogData,
+} from '../../ui/confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-article-details-page',
@@ -18,26 +32,58 @@ export class ArticleDetailsPage {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private firestore = inject(Firestore);
+  private toastr = inject(ToastrService);
+  private dialog = inject(MatDialog);
 
-  articleId: string;
   article$: Observable<IArticle | undefined>;
+  articleRef: DocumentReference<IArticle, DocumentData>;
 
   constructor() {
-    this.articleId = this.route.snapshot.paramMap.get('id')!;
-    const articleRef = doc(
+    const articleId = this.route.snapshot.paramMap.get('id')!;
+    this.articleRef = doc(
       this.firestore,
-      `articles/${this.articleId}`
+      `articles/${articleId}`
     ).withConverter(articleConverter);
-    this.article$ = docData(articleRef, {
+    this.article$ = docData(this.articleRef, {
       idField: 'id',
     });
   }
 
   editArticle() {
-    this.router.navigate(['articles', this.articleId, 'edit']);
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 
-  deleteArticle() {
-    // do zaimplementowania
+  onDelete() {
+    const data: ConfirmDialogData = {
+      title: 'Delete article',
+      content: 'Do you want to delete this article?',
+    };
+
+    const dialogRef = this.dialog.open<
+      ConfirmDialog,
+      ConfirmDialogData,
+      boolean
+    >(ConfirmDialog, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) {
+        return;
+      }
+
+      this.deleteArticle();
+    });
+  }
+
+  async deleteArticle() {
+    try {
+      await deleteDoc(this.articleRef);
+      this.toastr.success('Article deleted');
+      this.router.navigate(['../'], { relativeTo: this.route });
+    } catch (error) {
+      console.error(error);
+      this.toastr.error(GENERAL_ERROR_MESSAGE);
+    }
   }
 }
